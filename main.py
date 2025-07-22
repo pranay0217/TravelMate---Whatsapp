9f1a3234441a404d425331b277a606ceb1ce55d6
pranay0217 <shitpranay@gmail.com>
2025-07-22 13:10:27 +0530

    Update main.py

--- main.py
+++ main.py
@@ -123,4 +123,4 @@ async def whatsapp_webhook(
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run("main:app", host="0.0.0.0", port=os.getenv("PORT"), reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=int(os.getenv("PORT", 8000)), reload=True)
